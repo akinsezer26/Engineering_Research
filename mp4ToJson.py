e67c095f624a070a1e3ad17659f7852c781b39ae
celalormanli <ormanlicelal@gmail.com>
2018-12-17 04:04:16 +0300

    v6

--- mp4ToJson.py
+++ mp4ToJson.py
@@ -6,17 +6,17 @@ hareket1="ayaktaDurma"
 hareket2="Oturma"
 hareket3="volta"
 
-hareket=hareket3
+hareket=hareket2
 print os.getcwd()
-videoLocations = "/home/akin/openpose/train/"+str(hareket)
-os.chdir('/home/akin/openpose')
+videoLocations = "/home/celal/openpose/train/"+str(hareket)
+os.chdir('/home/celal/openpose')
 print os.getcwd()
 files=os.listdir(videoLocations)
 files.sort()
 
 for i in range(len(files)):
-	if not os.path.exists("/home/akin/openpose/train/JsonFiles/"+str(hareket)+"/"+str(i)):
-		os.makedirs("/home/akin/openpose/train/JsonFiles/"+str(hareket)+"/"+str(i))
+	if not os.path.exists("/home/celal/openpose/train/JsonFiles/"+str(hareket)+"/"+str(i)):
+		os.makedirs("/home/celal/openpose/train/JsonFiles/"+str(hareket)+"/"+str(i))
 	print(i)
 	terminalInput="./build/examples/openpose/openpose.bin --video train/"+hareket+"/"+ str(files[i]) +"  --write_json train/JsonFiles/"+str(hareket)+"/"+str(i)
 	os.system(terminalInput)

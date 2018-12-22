a30eeaa9e06e8670515203103bdde8a48536b28d
akin <akinsezer26@gmail.com>
2018-12-23 02:16:12 +0300

    v14

--- createModel.py
+++ createModel.py
@@ -4,6 +4,8 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 import os
+from keras import optimizers
+from keras import regularizers
 
 #read in data using pandas
 cwd = os.getcwd()
@@ -32,17 +34,23 @@ model = Sequential()
 #get number of columns in training data
 n_cols = train_X.shape[1]
 
+#early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=1, mode='auto')
+#model.add(Dense(52, input_dim=52,
+#            kernel_regularizer=regularizers.l2(0.01),
+#            activity_regularizer=regularizers.l1(0.01)))
 #add layers to model
-model.add(Dense(250, activation='sigmoid', input_shape=(n_cols,)))
-model.add(Dense(250, activation='sigmoid'))
-model.add(Dense(250, activation='sigmoid'))
+model.add(Dense(52, activation='sigmoid', input_shape=(n_cols,)))
+model.add(Dense(20, activation='sigmoid'))
 model.add(Dense(4, activation='softmax'))
 
+#opt=optimizers.SGD(lr=0.1, momentum=0.1)
 #compile model using accuracy to measure model performance
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 #train model
-model.fit(train_X, train_y, epochs=30, validation_split=0.2)
+model.fit(train_X, train_y, epochs=2000, validation_split=1)
+
+print(model.evaluate(train_X, train_y, verbose=0))
 
 # serialize model to JSON
 model_json = model.to_json()
